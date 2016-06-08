3829a77da963d39237fa6613ea35381474672e9e
André Ferrari <andferrari@gmail.com>
2016-06-08 16:02:33 +0200

    correct bug in dct

--- easy_muffinfunc.jl
+++ easy_muffinfunc.jl
@@ -1,25 +1,32 @@
 function easy_muffin(μ_s::Float64, μ_λ::Float64, nb::Int, nitermax::Int, file_in::AbstractString; filename_x0 = [])
 
   σ = 1.0
-  τ = 1e-5
+  τ = 1e-4
 
   # load psf, dirty, and sky
 
   println("loading...")
 
-  f = FITS(string(file_in,"_psf.fits"))
-  psf = read(f[1])[:,:,:]
+  folder = "/Users/ferrari/.julia/v0.4/MUFFIN_dev/data/tmp/"
+
+  f = FITS(string(folder,file_in,"_psf.fits"))
+  psf = read(f[1])
   close(f)
+  if length(size(psf))==4; psf = squeeze(psf,4);end
   psfadj = flipdim(flipdim(psf,2),1)
 
 
-  f = FITS(string(file_in,"_dirty.fits"))
-  dirty = read(f[1])[:,:,:]
+  f = FITS(string(folder,file_in,"_dirty.fits"))
+  dirty = read(f[1])
   close(f)
+  if length(size(dirty))==4; dirty = squeeze(dirty,4);end
 
-  f = FITS(string(file_in,".fits"))
-  sky = read(f[1])[:,:,:]
+
+  f = FITS(string(folder,file_in,"_sky.fits"))
+  sky = read(f[1])
   close(f)
+  if length(size(sky))==4; sky = squeeze(sky,4);end
+
 
   println("size psf ",size(psf))
   println("size dirty ",size(dirty))
@@ -66,22 +73,24 @@ function easy_muffin(μ_s::Float64, μ_λ::Float64, nb::Int, nitermax::Int, file
   println("iterate...")
 
   loop = true
+  niter = 0
+
   nrj_sky = vecnorm(sky)^2
   snr = Float64[]
   push!(snr,10*log10(nrj_sky/vecnorm(sky-x)^2))
-  println(0, " ", snr[1])
+  @printf("%03d | ", niter)
+  @printf("%02.04e \n", snr[1])
 
-  niter = 0
   while loop
     niter +=1
 
-    t = idct(v)
+    t = μ_λ*idct(v, 3)
 
     for freq in 1:nfreq
 
       # compute iuwt adjoint
       wstu = iuwt_recomp(squeeze(u[:,:,freq,:],3),0)
-      # wstu = iuwt_decomp_adj(u[:,:,freq,:], nb)
+      #wstu = IUWT.iuwt_decomp_adj(u[:,:,freq,:], nb)
 
       # compute gradient
       ∇_freq = real(ifftshift(ifft(fft(x[:,:,freq]).*hth_fft[:,:,freq]))) - fty[:,:,freq]
@@ -99,16 +108,16 @@ function easy_muffin(μ_s::Float64, μ_λ::Float64, nb::Int, nitermax::Int, file
     end
 
     # update v
-    v = sat(v + σ*μ_λ*dct(2*xt - x))
+    v = sat(v + σ*μ_λ*dct(2*xt - x, 3))
 
     # update x
     x = copy(xt)
 
     push!(snr, 10*log10(nrj_sky/vecnorm(sky-x)^2))
+    @printf("%03d | ", niter)
+    @printf("%02.04e \n", snr[niter + 1])
 
-    println(niter, " ", snr[niter+1])
-
-    if (niter >= nitermax)
+    if (niter == nitermax)
         loop = false
     end
 
@@ -117,6 +126,138 @@ function easy_muffin(μ_s::Float64, μ_λ::Float64, nb::Int, nitermax::Int, file
   return x, snr
 
 end
+
+function easy_muffin_c0(μ_s::Float64, μ_λ::Float64, nb::Int, nitermax::Int, file_in::AbstractString; filename_x0 = [])
+
+  σ = 1.0
+  τ = 1e-4
+
+  # load psf, dirty, and sky
+
+  println("loading...")
+
+  folder = "/Users/ferrari/.julia/v0.4/MUFFIN_dev/data/tmp/"
+
+  f = FITS(string(folder,file_in,"_psf.fits"))
+  psf = read(f[1])
+  close(f)
+  if length(size(psf))==4; psf = squeeze(psf,4);end
+  psfadj = flipdim(flipdim(psf,2),1)
+
+
+  f = FITS(string(folder,file_in,"_dirty.fits"))
+  dirty = read(f[1])
+  close(f)
+  if length(size(dirty))==4; dirty = squeeze(dirty,4);end
+
+
+  f = FITS(string(folder,file_in,"_sky.fits"))
+  sky = read(f[1])
+  close(f)
+  if length(size(sky))==4; sky = squeeze(sky,4);end
+
+
+  println("size psf ",size(psf))
+  println("size dirty ",size(dirty))
+  println("size sky ",size(sky))
+
+  nfreq = size(dirty,3)
+  nxy = size(dirty,1)
+
+  # initialize
+
+  if isempty(filename_x0)
+    x = init_dirty_admm(dirty, psf, psfadj, 5e1)
+  else
+    x = JLD.load(x0,"x")
+  end
+
+  u = zeros(nxy, nxy, nfreq, nb+1)
+  v = zeros(nxy, nxy, nfreq)
+
+  # precomputations
+  println("precomputations...")
+
+  psfadj_fft = Array(Complex128,nxy,nxy)
+  hth_fft = Array(Complex128,nxy,nxy,nfreq)
+  fty = Array(Float64,nxy,nxy,nfreq)
+
+  for k in 1:nfreq
+    psfadj_fft = fft(psfadj[:,:,k])
+    hth_fft[:,:,k] = fft(ifftshift(ifft(psfadj_fft.*fft(psf[:,:,k]))))
+    fty[:,:,k] = real(ifftshift(ifft(fft(dirty[:,:,k]).*psfadj_fft)))
+  end
+
+  wstu = Array(Float64,nxy,nxy)
+  ∇_freq = Array(Float64,nxy,nxy)
+  tmp_spat_cal = Array(Float64,nxy,nxy,nb)
+  xt = Array(Float64,nxy,nxy,nfreq)
+
+  # free memory
+  psf = 0
+  psfadj = 0
+
+  # Iterations
+
+  println("iterate...")
+
+  loop = true
+  niter = 0
+
+  nrj_sky = vecnorm(sky)^2
+  snr = Float64[]
+  push!(snr,10*log10(nrj_sky/vecnorm(sky-x)^2))
+  @printf("%03d | ", niter)
+  @printf("%02.04e \n", snr[1])
+
+  while loop
+    niter +=1
+
+    t = idct(v, 3)
+
+    for freq in 1:nfreq
+
+      # compute iuwt adjoint
+      wstu = iuwt_recomp(squeeze(u[:,:,freq,1:nb],3),0,c0=u[:,:,freq,nb+1])
+
+      # compute gradient
+      ∇_freq = real(ifftshift(ifft(fft(x[:,:,freq]).*hth_fft[:,:,freq]))) - fty[:,:,freq]
+
+      # compute xt
+      xt[:,:,freq] = max(x[:,:,freq] - τ*(∇_freq + μ_s*wstu + μ_λ*t[:,:,freq]), 0.0 )
+
+      # update u
+      coef, c0 = iuwt_decomp(2*xt[:,:,freq] - x[:,:,freq], nb, store_c0 = true)
+      tmp_spat_scal = cat(3, coef, c0)
+
+      size(tmp_spat_scal)
+      size(u)
+      for b in 1:nb+1
+          u[:,:,freq,b] = sat(u[:,:,freq,b] + σ*μ_s*tmp_spat_scal[:,:,b])
+      end
+
+    end
+
+    # update v
+    v = sat(v + σ*μ_λ*dct(2*xt - x, 3))
+
+    # update x
+    x = copy(xt)
+
+    push!(snr, 10*log10(nrj_sky/vecnorm(sky-x)^2))
+    @printf("%03d | ", niter)
+    @printf("%02.04e \n", snr[niter + 1])
+
+    if (niter == nitermax)
+        loop = false
+    end
+
+  end
+
+  return x, snr
+
+end
+
 
 
 function init_dirty_admm(dirty::Array{Float64,3},psf::Array{Float64,3},psfadj::Array{Float64,3},mu)
@@ -144,26 +285,10 @@ function init_dirty_admm(dirty::Array{Float64,3},psf::Array{Float64,3},psfadj::A
 end
 
 
-function iuwt_decomp_adj(u1,tmp1,scale)
-
-    tmp1[:,:,1,1] = iuwt_decomp(u1[:,:,1,1],1)[:,:,1]
-        for k in 2:scale
-            tmp1[:,:,1,1] += iuwt_decomp(u1[:,:,1,k],k)[:,:,k]
-        end
-    return tmp1
-end
-
 function sat(x::Array{Float64,3})
     min(abs(x), 1.0).*sign(x)
 end
 
 function sat(x::Array{Float64,2})
     min(abs(x), 1.0).*sign(x)
-end
-
-function hthx(x::Array{Float64,2},psf_fft::Array{Complex128,2},psf_adj_fft::Array{Complex128,2},)
-
-    planifft*((planfft*x[:,:,freq]).*psf_fft[:,:,freq])
-    tmp_spat_2 = real(planifft*((planfft*tmp_spat_2).*psfadj_fft[:,:,freq])) - fty[:,:,freq]
-
 end
--- run_muffin.jl
+++ run_muffin.jl
@@ -4,16 +4,17 @@ using IUWT
 
 include("easy_muffinfunc.jl")
 
-μ_λ = 0.0 #3.0
-μ_s = 0.25
-nb = 8
-nitermax = 4000
-file_in = "m31_3d"
+#num_simu = parse(Int, ARGS[1]) # for loop
+num_simu = 2
+μ_s = logspace(-3,0,10)[num_simu]
 
-num_simu = parse(Int, ARGS[1])
-μ_s = logspace(-4,1,20)[num_simu]
+μ_λ = 0.3 #3.0
+nb = 8
+nitermax = 10
+file_in = "m31_3d_small"
 
 x, snr = easy_muffin(μ_s, μ_λ, nb, nitermax, file_in)
+#x, snr_c0 = easy_muffin_c0(μ_s, μ_λ, nb, nitermax, file_in)
 
-file_out = string("results/x_init_","$nitermax","_",@sprintf("%01.03f", μ_s),"_",@sprintf("%01.03f", μ_λ),".jld")
-JLD.save(file_out, "snr", snr,"μ_s",μ_s,"μ_λ",μ_λ)
+#file_out = string("results/x_init_","$nitermax","_",@sprintf("%01.03f", μ_s),"_",@sprintf("%01.03f", μ_λ),".jld")
+#JLD.save(file_out, "snr", snr,"μ_s",μ_s,"μ_λ",μ_λ)
