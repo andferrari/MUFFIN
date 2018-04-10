c1f26bf85552869e7c227a055db45d19d0d74d5a
Alain Miniussi <alain.miniussi@oca.eu>
2018-04-10 22:31:39 +0200

    Add datacheck at the end of test refs #21

--- easy_muffin_py/test/test1/run.sh
+++ easy_muffin_py/test/test1/run.sh
@@ -8,3 +8,4 @@ mkdir -p $mydir/../../output_sigamm/$$
 
 mpirun -n 8 $mydir/../../run_tst_mpi_sigamm.py -L 19 -N 2 -mu_s 1 -mu_l 10 -mu_w 10 -stp_s 0.5 -stp_l 100 -pxl_w 1 -bnd_w 1 -data M31_skyline2_20db -fol $mydir/data
 
+$mydir/../../compare_muffin_outputs.py --gold=$mydir/gold --out=$mydir/../../output_sigamm/$$
