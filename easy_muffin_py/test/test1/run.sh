37476a67aa6d32e38c5b92f7eaf047d99a4718e1
Alain Miniussi <alain.miniussi@oca.eu>
2018-04-12 16:34:30 +0200

    add verbosity

--- easy_muffin_py/test/test1/run.sh
+++ easy_muffin_py/test/test1/run.sh
@@ -1,5 +1,29 @@
 #!/usr/bin/env bash
 
+nbproc=8
+while getopts "n:cv" opt
+do
+    case $opt in
+	n)
+	    nbproc=$OPTARG
+	    ;;
+	c)
+	    coverage="coverage run"
+	    ;;
+	v)
+	    verbose="-v"
+	    ;;
+	\?)
+	    echo "invald option -$OPTARG." >&2
+	    exit 1
+	    ;;
+	:)
+	    echo "option -$OPTARG requires an argument." >&2
+	    exit 1
+	    ;;
+    esac
+done
+      
 mydir=$(dirname $(realpath $0))
 
 odir=$mydir/output_sigamm/$$
@@ -7,7 +31,14 @@ mkdir -pv $odir
 echo "output directory is $odir"
 echo "log will be in $odir/muffin.log"
 
-mpirun -n 8 $mydir/../../run_tst_mpi_sigamm.py -L 19 -N 2 -mu_s 1 -mu_l 10 -mu_w 10 -stp_s 0.5 -stp_l 100 -pxl_w 1 -bnd_w 1 -data M31_skyline2_20db -fol $mydir/data --odir $odir 2>&1 | tee $odir/muffin.log
+mpirun -n $nbproc $mydir/../../run_tst_mpi_sigamm.py -L 19 -N 2 -mu_s 1 -mu_l 10 -mu_w 10 -stp_s 0.5 -stp_l 100 -pxl_w 1 -bnd_w 1 -data M31_skyline2_20db -fol $mydir/data --odir $odir 2>&1 | tee $odir/muffin.log
 
+$mydir/../../compare_muffin_outputs.py --gold=$mydir/gold --out=$odir --ratio 1e-14 --diff 1e-16 --cool $verbose
+status=$?
+if [ -n "$coverage" ]
+then
+    coverage report -m
+    coverage html
+fi
 
-$mydir/../../compare_muffin_outputs.py --gold=$mydir/gold --out=$odir --ratio 1e-14 --diff 1e-16 --cool 
+exit $status
