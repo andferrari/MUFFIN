61c5ba2d836a36d057826fae5ece7fb3a9be40a7
Alain Miniussi <alain.miniussi@oca.eu>
2018-04-12 17:48:10 +0200

    Add coverage refs #17 and #21

--- easy_muffin_py/test/test1/run.sh
+++ easy_muffin_py/test/test1/run.sh
@@ -8,7 +8,7 @@ do
 	    nbproc=$OPTARG
 	    ;;
 	c)
-	    coverage="coverage run"
+	    coverage="coverage run --parallel-mode"
 	    ;;
 	v)
 	    verbose="-v"
@@ -31,7 +31,7 @@ mkdir -pv $odir
 echo "output directory is $odir"
 echo "log will be in $odir/muffin.log"
 
-mpirun -n $nbproc $mydir/../../run_tst_mpi_sigamm.py -L 19 -N 2 -mu_s 1 -mu_l 10 -mu_w 10 -stp_s 0.5 -stp_l 100 -pxl_w 1 -bnd_w 1 -data M31_skyline2_20db -fol $mydir/data --odir $odir 2>&1 | tee $odir/muffin.log
+mpirun -n $nbproc $coverage $mydir/../../run_tst_mpi_sigamm.py -L 19 -N 2 -mu_s 1 -mu_l 10 -mu_w 10 -stp_s 0.5 -stp_l 100 -pxl_w 1 -bnd_w 1 -data M31_skyline2_20db -fol $mydir/data --odir $odir 2>&1 | tee $odir/muffin.log
 
 $mydir/../../compare_muffin_outputs.py --gold=$mydir/gold --out=$odir --ratio 1e-14 --diff 1e-16 --cool $verbose
 status=$?
